c6618ce03ee1119549edc53dcd68decc50d4db5d
Arne Brasseur <arne@arnebrasseur.net>
2014-03-16 19:06:19 +0100

    Stop the tests from overwriting ~/.analects

--- spec/analects/library_spec.rb
+++ spec/analects/library_spec.rb
@@ -1,19 +1,19 @@
 require 'tempfile'
-require 'spec_helper'
 require 'securerandom'
+require 'pathname'
+
+require 'spec_helper'
 
 describe Analects::Library do
   subject(:library) {
     described_class.new(options)
   }
-  let(:options) { {} }
+  let(:data_dir) { File.join(Dir.tmpdir, 'analects-' + SecureRandom.hex(16)) }
+  let(:options) {
+    { data_dir: data_dir }
+  }
 
   context 'with a data_dir specified' do
-    let(:data_dir) { File.join(Dir.tmpdir, 'analects-' + SecureRandom.hex(16)) }
-    let(:options) {
-      { data_dir: data_dir }
-    }
-
     it 'should set that data dir on the sources' do
       subject.cedict.data_dir.should == data_dir
     end
@@ -23,7 +23,6 @@ describe Analects::Library do
     subject(:cedict) { library.cedict }
 
     its(:name)     { should == :cedict }
-    its(:location) { should == File.join(Dir.home, '.analects/cedict_1_0_ts_utf-8_mdbg.txt') }
 
     it "should download and unpack the CEDICT archive" do
       cedict.should_receive(:retrieve_http).once.with(Analects::CEDICT_URL).and_return(:a_stream)
@@ -42,7 +41,6 @@ describe Analects::Library do
 
     its( :name )      { should == :chise_ids }
     its( :retrieval ) { should == [ :git ] }
-    its(:location   ) { should == File.join(Dir.home, '.analects/chise_ids') }
     its( :url       ) { should == Analects::CHISE_IDS_URL}
   end
 
