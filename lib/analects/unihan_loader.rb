16c59730f22aa3070fbd14cc14b7b26a0a3e94e6
Arne Brasseur <arne@arnebrasseur.net>
2014-12-17 17:50:13 +0100

    Some work on the UnihanLoader
    Stuff I did in May and apparently never committed, not sure what state
    this is in.

--- lib/analects/unihan_loader.rb
+++ lib/analects/unihan_loader.rb
@@ -3,5 +3,36 @@
 module Analects
   class UnihanLoader
     include Enumerable
+
+    attr_reader :location
+
+    def initialize(location, library)
+      @location = location
+    end
+
+    REGEXP = %r{U\+(?<codepoint>[0-9A-F]+)  # U+2B7C7
+                \t
+                (?<field>k[^\t]+)          # kTotalStrokes
+                \t
+                (?<value>.*)}x              # 17
+
+    def each
+      return to_enum(__method__) unless block_given?
+      files.each do |file|
+        file.each_line do |line|
+          next unless matchdata = line.match(REGEXP)
+          yield(
+            :codepoint => matchdata[:codepoint].hex,
+            :char => [matchdata[:codepoint].hex].pack('U'),
+            :field => matchdata[:field],
+            :value => matchdata[:value]
+          )
+        end
+      end
+    end
+
+    def files
+      @location.children
+    end
   end
 end
---
+++ spec/analects/unihan_loader_spec.rb
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe Analects::UnihanLoader do
+  subject(:loader) { described_class.new(fake_directory_pathname, nil) }
+  let(:fake_directory_pathname) {
+    Class.new do
+      def children ; [self] end
+      def each_line(&blk)
+        [ 'U+2B7C7	kTotalStrokes	17',
+          'U+343C	kRSUnicode	9.4',
+          'U+2F9FC	kCompatibilityVariant	U+4AB2'
+        ].each(&blk)
+      end
+    end.new
+  }
+
+  describe '#each' do
+    it 'should yield once for each entry' do
+      expect(subject.count).to eql 3
+    end
+
+    it 'should pull out the individual fields' do
+      expect(subject.first.values_at(:codepoint, :field, :value)).to eq [0x2B7C7, 'kTotalStrokes', '17']
+    end
+
+    it 'should add a character for the codepoint' do
+      expect(subject.first[:char]).to eql '𫟇'
+    end
+  end
+
+  describe "REGEXP" do
+    let(:entry) { "U+2B7C7\tkTotalStrokes\t17\n" }
+    let(:matchdata) { entry.match(Analects::UnihanLoader::REGEXP) }
+
+    it 'should match valid lines' do
+      expect(entry).to match Analects::UnihanLoader::REGEXP
+    end
+
+    it 'should extract the data' do
+      expect(matchdata[:codepoint]).to eql "2B7C7"
+      expect(matchdata[:field]).to eql "kTotalStrokes"
+      expect(matchdata[:value]).to eql "17"
+    end
+
+
+
+  end
+end
